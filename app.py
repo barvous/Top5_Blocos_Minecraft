231cafab7ab8909bd3875bd5d79ab827c119f990
Marcos Daniel <marcos.coelho@wmtrading.com.br>
2021-12-15 18:01:28 -0300

    estilização padrão das telas

--- app.py
+++ app.py
@@ -8,5 +8,26 @@ def index():
     return render_template('index.html')
 
 
+def top5():
+    return render_template('top5.html')
+app.add_url_rule("/top5","top5", top5)
+
+def top4():
+    return render_template('top4.html')
+app.add_url_rule("/top4","top4", top4)
+
+def top3():
+    return render_template('top3.html')
+app.add_url_rule("/top3","top3", top3)
+
+def top2():
+    return render_template('top2.html')
+app.add_url_rule("/top2","top2", top2)
+
+def top1():
+    return render_template('top1.html')
+app.add_url_rule("/top1","top1", top1)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
